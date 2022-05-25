ecb2a74fbdd81c86cd8a479bdf071b5621a02e49
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-05-25 13:03:42 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    buttonDisabled: true,
+    search: '',
+  };
+
+  buttonSearchValidate = () => {
+    const { search } = this.state;
+    const valid = 2;
+    if (search.length >= valid) {
+      this.setState({ buttonDisabled: false });
+    }
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value }, this.buttonSearchValidate);
+  }
+
   render() {
+    const { buttonDisabled, search } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="search"
+            search={ search }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            type="text"
+          />
+          <button
+            disabled={ buttonDisabled }
+            data-testid="search-artist-button"
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
