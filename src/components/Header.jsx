9733327b4ef4b9e27760be5cb0dd940edff944fc
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-05-25 12:30:41 -0300

    requisito 4 feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -20,6 +21,9 @@ render() {
   const { user, loadingPage } = this.state;
   return loadingPage ? <Loading /> : (
     <header data-testid="header-component">
+      <Link data-testid="link-to-search" to="/search">Ir para pesquisa</Link>
+      <Link data-testid="link-to-favorites" to="/favorites">Ir para favoritos</Link>
+      <Link data-testid="link-to-profile" to="/profile">Ir para perfil</Link>
       <p data-testid="header-user-name">
         {user}
       </p>
